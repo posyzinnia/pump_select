6503969c3eabfb7de8bb1e3974f3857fc700f19a
posyzinnia <posyzinnia@gmail.com>
2026-06-15 14:27:54 +0700

    Update script.js

--- renderData.js/script.js
+++ renderData.js/script.js
@@ -214,9 +214,6 @@ function renderPumpCards(totalHead) {
                     <div class="pump-card-flow-badge">
                         ≈ ${estimatedFlow !== null ? estimatedFlow.toFixed(2) + " ม³/ชม." : "-"}
                     </div>
-                    <div class="pump-card-flow-badge">
-                        ${price}
-                    </div>
                 `;
     card.addEventListener("click", () => selectPumpCard(name, card));
     grid.appendChild(card);
